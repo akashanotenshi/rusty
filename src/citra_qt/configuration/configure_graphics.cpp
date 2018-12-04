393b1047097720c2c8f3acb8e7dc725d9647060b
sickc <bobcat24.zhou@gmail.com>
2018-12-04 11:08:23 +0800

    valentinvanelslande's screen refresh rate setting

--- src/citra_qt/configuration/configure_graphics.cpp
+++ src/citra_qt/configuration/configure_graphics.cpp
@@ -20,6 +20,10 @@ ConfigureGraphics::ConfigureGraphics(QWidget* parent)
     ui->frame_limit->setEnabled(Settings::values.use_frame_limit);
     connect(ui->toggle_frame_limit, &QCheckBox::stateChanged, ui->frame_limit,
             &QSpinBox::setEnabled);
+    ui->AddTicks->setEnabled(Settings::values.FMV_hack);
+    connect(ui->FMV_hack, &QCheckBox::stateChanged, ui->AddTicks, &QSpinBox::setEnabled);
+    ui->screen_refresh_rate->setEnabled(Settings::values.custom_refresh_rate);
+    connect(ui->custom_refresh_rate, &QCheckBox::stateChanged, ui->screen_refresh_rate, &QSpinBox::setEnabled);
 
     ui->layoutBox->setEnabled(!Settings::values.custom_layout);
 
@@ -80,6 +84,8 @@ void ConfigureGraphics::setConfiguration() {
     ui->bg_button->setIcon(color_icon);
     ui->FMV_hack->setChecked(Settings::values.FMV_hack);
     ui->AddTicks->setValue(Settings::values.AddTicks);
+    ui->custom_refresh_rate->setChecked(Settings::values.custom_refresh_rate);
+    ui->screen_refresh_rate->setValue(Settings::values.screen_refresh_rate);
 }
 
 void ConfigureGraphics::applyConfiguration() {
@@ -103,6 +109,8 @@ void ConfigureGraphics::applyConfiguration() {
     Settings::values.bg_blue = static_cast<float>(bg_color.blueF());
     Settings::values.FMV_hack = ui->FMV_hack->isChecked();
     Settings::values.AddTicks = ui->AddTicks->value();
+    Settings::values.custom_refresh_rate = ui->custom_refresh_rate->isChecked();
+    Settings::values.screen_refresh_rate = ui->screen_refresh_rate->value();
 }
 
 void ConfigureGraphics::retranslateUi() {
